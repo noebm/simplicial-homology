78338a034691482b3c30f62284a7a4398f23a54d
noebm <moritz.noebauer@gmail.com>
2022-06-14 19:18:23 +0200

    Replace SimplexTree structure

--- app/Main.hs
+++ app/Main.hs
@@ -2,49 +2,49 @@ module Main where
 
 import Lib
 import Data.List (group, sort, find)
-import System.IO.Unsafe (unsafePerformIO)
+import Data.Tree
 
-newtype SimplexTree a = SimplexTree [(a,[SimplexTree a])]
+newtype SimplexTree a = SimplexTree (Forest a)
   deriving (Show)
 
 instance Functor SimplexTree where
-  fmap f (SimplexTree xs) = SimplexTree $ fmap aux xs
-    where
-    aux (y , ts) = (f y, fmap (fmap f) ts)
+  fmap f (SimplexTree xs) = SimplexTree $ fmap (fmap f) xs
+
+draw :: Show a => SimplexTree a -> String
+draw (SimplexTree xs) = drawForest $ fmap (fmap show) xs
 
 newtype Simplex a = Simplex [a]
   deriving (Show, Ord, Eq)
+
+prepend :: a -> Simplex a -> Simplex a
+prepend v (Simplex vs) = Simplex (v : vs)
 
 -- insert :: Ord a => Simplex a -> SimplexTree a -> SimplexTree a
 -- insert (Simplex []) t = t
 -- insert (Simplex (v : vs)) (SimplexTree t) = SimplexTree (as ++ (v, insert (Simplex vs) s) : b)
 --   where (as, bs) = break ((>= v) . fst) $ t
 
-simplices :: SimplexTree a -> [ Simplex a ]
-simplices (SimplexTree t) = do
-  (v, ts) <- t
-  (Simplex [v] :) $ do
-    Simplex vs <- concat $ simplices <$> ts
-    return $ Simplex (v : vs)
+simplices :: SimplexTree a -> [Simplex a]
+simplices (SimplexTree xs) = go =<< xs where
+  go :: Tree a -> [ Simplex a ]
+  go = foldTree (\v f -> Simplex [v] : (prepend v <$> concat f))
+
 
 -- disconnected simplial 0-complex of size 2
 test0 :: SimplexTree Int
-test0 = SimplexTree [(0, []), (1, [])]
+test0 = SimplexTree [Node 0 [], Node 1 []]
 
 test1 :: SimplexTree Int
-test1 = SimplexTree [(0, [SimplexTree [(1, [])]]), (1, [])]
+test1 = SimplexTree [Node 0 [Node 1 []], Node 1 []]
 
-complete' :: Int -> SimplexTree Int
-complete' = go 0 where
+complete :: Int -> SimplexTree Int
+complete = SimplexTree . go where
 
-  go :: Int -> Int -> SimplexTree Int
-  go k n | k < 0 = SimplexTree []
-  go k n = SimplexTree $ do
-    l <- [k..n]
-    return (l, [ go i n | i <- [l+1..n] ])
+  go :: Int -> [ Tree Int ]
+  go n = unfoldForest (\l -> (l, [l+1..n])) [0..n]
 
 completeOrd :: Ord a => [a] -> SimplexTree a
-completeOrd xs = (xs' !!) <$> complete' (length xs' - 1)
+completeOrd xs = (xs' !!) <$> complete (length xs' - 1)
   where xs' = map head . group . sort $ xs
 
 main :: IO ()
@@ -56,13 +56,13 @@ main = do
   print test1
 
   putStrLn "complete 0"
-  print (complete' 0)
+  print (complete 0)
 
   putStrLn "complete 1"
-  print (complete' 1)
+  print (complete 1)
 
   putStrLn "complete 2"
-  print (complete' 2)
+  print (complete 2)
 
   putStrLn "completeOrd \"abc\""
   print (completeOrd "abc")
