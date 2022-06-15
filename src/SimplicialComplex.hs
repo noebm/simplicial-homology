84d25829c2df458ef9bc70a4e5202968db011eff
noebm <moritz.noebauer@gmail.com>
2022-06-16 00:43:35 +0200

    Add insertion of simplices to simplicial complex

--- src/SimplicialComplex.hs
+++ src/SimplicialComplex.hs
@@ -5,7 +5,10 @@ import Data.List
 import Simplex
 
 newtype SimplicialComplex a = SimplicialComplex (Forest a)
-  deriving (Show)
+  deriving (Show, Eq)
+
+emptyComplex :: SimplicialComplex a
+emptyComplex = SimplicialComplex []
 
 instance Functor SimplicialComplex where
   fmap f (SimplicialComplex xs) = SimplicialComplex $ fmap (fmap f) xs
@@ -17,6 +20,26 @@ simplices :: SimplicialComplex a -> [Simplex a]
 simplices (SimplicialComplex xs) = go =<< xs where
   go :: Tree a -> [ Simplex a ]
   go = foldTree (\v f -> Simplex [v] : (prepend v <$> concat f))
+
+simplexComplex :: Simplex a -> SimplicialComplex a
+simplexComplex (Simplex s) = SimplicialComplex . go $ s where
+  go (s0:s) = Node s0 (go s): go s
+  go [] = []
+
+insert :: Ord a => Simplex a -> SimplicialComplex a -> SimplicialComplex a
+insert (Simplex s) (SimplicialComplex sc) = SimplicialComplex $ go s sc where
+
+  go :: Ord a => [a] -> Forest a -> Forest a
+  go s [] | SimplicialComplex sc <- simplexComplex (Simplex s) = sc
+  go [] sc = sc
+  go s@(s0:s') sc@(Node v t0 : sc') =
+    case s0 `compare` v of
+      LT | SimplicialComplex sc0 <- simplexComplex (Simplex s) -> sc0 ++ sc
+      EQ -> Node v (go s' t0) : go s' sc'
+      GT -> head sc : go s sc'
+
+fromSimplices :: Ord a => [Simplex a] -> SimplicialComplex a
+fromSimplices = foldr SimplicialComplex.insert emptyComplex
 
 complete :: Int -> SimplicialComplex Int
 complete = SimplicialComplex . go where
