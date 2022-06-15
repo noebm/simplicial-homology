01a2670602810a1360fcd0177917b46ea18eb42e
noebm <moritz.noebauer@gmail.com>
2022-06-16 01:13:24 +0200

    Fix Simplex arbitrary instance

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,14 +3,18 @@
 import Lib
 import Test.QuickCheck
 
+prop_SimplexOrdered :: Ord a => Simplex a -> Bool
+prop_SimplexOrdered (Simplex []) = True
+prop_SimplexOrdered (Simplex xs) = all (uncurry (<)) $ zip (init xs) (tail xs)
+
 prop_complexId :: (Eq a, Ord a) => SimplicialComplex a -> Bool
 prop_complexId sc = fromSimplices (simplices sc) == sc
 
 prop_simplexId :: Ord a => [Simplex a] -> Bool
 prop_simplexId s = simplices (fromSimplices s) >= s
 
-instance Arbitrary a => Arbitrary (Simplex a) where
-  arbitrary = Simplex <$> arbitrary
+instance (Ord a, Arbitrary a) => Arbitrary (Simplex a) where
+  arbitrary = fromList <$> arbitrary
 
 instance (Ord a, Arbitrary a) => Arbitrary (SimplicialComplex a) where
   arbitrary = fromSimplices <$> arbitrary
