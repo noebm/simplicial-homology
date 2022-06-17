9f8bdeeb72ed23bf7e321d182561be35d23c2e64
noebm <moritz.noebauer@gmail.com>
2022-06-18 01:45:49 +0200

    Implement simplicial homology calculation

--- src/SimplicialComplex.hs
+++ src/SimplicialComplex.hs
@@ -6,6 +6,10 @@ import Data.Maybe
 import Data.List
 import Simplex
 
+import qualified Data.Matrix as M
+import Data.Matrix.SmithNormalForm
+import qualified Data.Vector as V
+
 newtype SimplicialComplex a = SimplicialComplex { simplexTree :: Forest a }
   deriving (Show, Eq)
 
@@ -104,27 +108,53 @@ data Boundary a = Boundary {
 } deriving (Show)
 
 boundaryBasis :: Eq a => [a] -> [a] -> Boundary a -> Boundary Int
-boundaryBasis facets cells (Boundary c fs) = Boundary (get cells c) (get facets <$> fs)
+boundaryBasis facets cells (Boundary c fs) = Boundary (get cells c) (if null facets then [] else get facets <$> fs)
   where get basis = fromJust . flip elemIndex basis
 
-type AssocMatrix a = (Int, Int, [(Int, Int, a)])
+type AssocMatrix a = (Int, Int, [((Int, Int), a)])
 
 assocMatrix :: Num a => AssocMatrix a -> (Int, Int, [ a ])
 assocMatrix (m, n, xs) =
-  let ys = sortBy (\(i,j,_) (i',j',_) -> (i,j) `compare` (i',j')) xs
-      go (i,j) (i',j',v) = let k = (i' - i) * n + (j' - j) -1
+  let ys = sortBy (\(pos,_) (pos',_) -> pos `compare` pos') xs
+      go (i,j) ((i',j'),v) = let k = (i' - i) * n + (j' - j) -1
                             in ((i',j'), replicate k 0 ++ [v])
    in (,,) m n $ concat $ snd $ mapAccumL go (0,0) ys
 
 incidences :: (Eq a, Num b) => SimplicialComplex a -> [AssocMatrix b]
-incidences sc = go (allCells sc) where
+incidences sc = go $ [] : tail (allCells sc) where
   boundaryMap cell = Boundary cell (boundary cell)
 
   aux face cell = do
     Boundary j is <- boundaryBasis face cell . boundaryMap <$> cell
     (i, v)  <- zip is (cycle [1, -1])
-    return (i, j, v)
+    return ((i, j), v)
 
   go list = do
     (face, cell) <- zip list (tail list)
     return $ (,,) (length face) (length cell) $ aux face cell
+
+boundaries :: Eq a => SimplicialComplex a -> [ M.Matrix Int ]
+boundaries = fixZero . fmap toMatrix . incidences where
+  -- most likely terrifingly slow!
+  toMatrix (m, n, assoc) = M.matrix m n $ \(i,j) -> fromMaybe 0 ((i-1,j-1) `lookup` assoc)
+
+  -- remove C0 -> C-1 map
+  -- matrix cannot handle matrices with zero rows/cols
+  fixZero = tail
+
+data HomologyFactors = HomologyFactors { free :: Int, torsion :: V.Vector Int }
+  deriving (Show, Eq)
+
+-- TODO does not work for 0-simplices since the dimensions are infered by boundary maps
+homology :: Eq a => SimplicialComplex a -> [ HomologyFactors ]
+homology sc = go (insertZeroMap smith) where
+  -- information about image and domain dimensionality and rank
+  smith = fmap (extract . smithNormalForm) . boundaries $ sc
+  extract m = (M.nrows m, M.ncols m, V.filter (/= 0) $ M.getDiag m)
+
+  insertZeroMap xs =
+    let (m,_,_) = head xs
+        (_,n,_) = last xs
+     in (0, m, V.empty):xs ++ [(n,0,V.empty)]
+  go xs = zipWith aux xs (tail xs)
+  aux (_,_, rb) (dim, _, ra) = HomologyFactors (dim - V.length ra - V.length rb) ra
